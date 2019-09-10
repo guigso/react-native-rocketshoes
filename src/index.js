63327fd2d2b62e5af83330067463346d4fc487d1
guigso <gui.soliveira@hotmail.com>
2019-09-09 22:06:10 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+};
+
+export default App;
